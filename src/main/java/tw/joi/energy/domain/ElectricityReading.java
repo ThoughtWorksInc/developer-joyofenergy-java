4540ae0e2f8d9457bece58f1bd8bb9826d8250d4
Matteo Vaccari <mvaccari@thoughtworks.com>
2024-06-06 10:01:02 +0200

    Change comment that the electricity reading is in KWh not KW

--- src/main/java/tw/joi/energy/domain/ElectricityReading.java
+++ src/main/java/tw/joi/energy/domain/ElectricityReading.java
@@ -4,6 +4,6 @@ import java.math.BigDecimal;
 import java.time.Instant;
 
 /**
- * @param reading kW
+ * @param reading KWh
  */
 public record ElectricityReading(Instant time, BigDecimal reading) {}
