17c277a14a40af6f8111ad341810492f5b7f9f59
Tanuj Mathur <tanuj.mathur@thoughtworks.com>
2024-06-10 14:10:57 +0200

    explicitly declare JUnit5 deepndencies to fix gradle 9 compatibility warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ repositories {
 
 dependencies {
     testImplementation("org.assertj:assertj-core:3.26+")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 application {
